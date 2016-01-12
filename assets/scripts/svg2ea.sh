ab055d97acc38d880704dc8d1179fde809ef8af5
russell <russell.davies@blakemere.ca>
2016-01-12 11:59:56 -0500

    Fix #2: Icons corrupted.  Changed how the BMPs are generated, forcing old-style 24-bit BMPs

--- assets/scripts/svg2ea.sh
+++ assets/scripts/svg2ea.sh
@@ -12,12 +12,13 @@ do
 
   echo -n "Converting $line... "
 
-  # Convert to EMF & WMF formats, which are useful as alternative images for elements.
-#  inkscape --file "$file" --export-area-drawing --export-wmf "${file%.svg}.wmf"
+  # Convert to EMF format, which are useful as alternative images for elements.
   inkscape --file "$file" --export-area-drawing --export-emf "${file%.svg}.emf"
 
   # Convert to 16x16 bitmaps, which is useful for icons in toolboxes and the project browser. 
-  convert "$file" -auto-orient -thumbnail "16x16>" -background white -gravity center -extent 16x16 -unsharp 0x3+1.5+0.01963+1.5+0.01963+1.5+0.0196 "${file%.*}.bmp"
+  # Note, EA doesn't support 32-bit BMPs, and convert seems to not produce 24-bit bmps, so we use a BMP3: to
+  # force use of old style BMPs.    
+  convert "$file" -auto-orient -thumbnail "16x16>" -background white -gravity center -extent 16x16 -unsharp 0x3+1.5+0.01963+1.5+0.01963+1.5+0.0196 BMP3:"${file%.*}.bmp"
   
   echo "Done"
   popd &> /dev/null
